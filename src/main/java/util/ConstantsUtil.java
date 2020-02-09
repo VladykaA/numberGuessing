462c115dd70a9e5e990ed7a7b04cc93f93cdaa7e
aleksandr.wladyka@gmail.com <Qwerty77+7>
2020-02-09 20:04:06 +0200

    update

---
+++ src/main/java/util/ConstantsUtil.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.util.Random;
+
+public class ConstantsUtil {
+    public static final Random RANDOM = new Random();
+
+    public static final int MAX_VALUE = 101;
+    public static final int MIN = 0;
+
+    public static final String WELCOME = "I want to play a game.";
+    public static final String WRONG_ANSWER = "You failed.";
+    public static final String RIGHT_ANSWER = "Excellent! You win";
+    public static final String ANSWER = "The correct number is: ";
+    public static final String ATTEMPTS = "Your answers: ";
+    public static final String WRONG_INPUT = "Wrong input. Please, try again.";
+    public static final String THE_RANGE_FROM = "The range is ]";
+    public static final String THE_RANGE_TO = "[ ";
+
+}
